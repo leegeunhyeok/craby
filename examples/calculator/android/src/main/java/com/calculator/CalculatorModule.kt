ceacf258846224424d6d6fd48decd56993f1aeb7
leegeunhyeok <dev.ghlee@gmail.com>
2025-07-19 21:46:46 +0900

    chore: add cacluator module

---
+++ examples/calculator/android/src/main/java/com/calculator/CalculatorModule.kt
@@ -0,0 +1,23 @@
+package com.calculator
+
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.module.annotations.ReactModule
+
+@ReactModule(name = CalculatorModule.NAME)
+class CalculatorModule(reactContext: ReactApplicationContext) :
+  NativeCalculatorSpec(reactContext) {
+
+  override fun getName(): String {
+    return NAME
+  }
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  override fun multiply(a: Double, b: Double): Double {
+    return a * b
+  }
+
+  companion object {
+    const val NAME = "Calculator"
+  }
+}
---
+++ examples/calculator/src/index.tsx
@@ -0,0 +1,5 @@
+import Calculator from './NativeCalculator';
+
+export function multiply(a: number, b: number): number {
+  return Calculator.multiply(a, b);
+}
