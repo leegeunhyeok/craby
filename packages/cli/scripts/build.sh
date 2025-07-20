fc920057b1d249557de2c54befa750040b866135
leegeunhyeok <dev.ghlee@gmail.com>
2025-07-21 00:19:29 +0900

    add napi code

--- packages/cli/scripts/build.sh
+++ packages/cli/scripts/build.sh
@@ -2,14 +2,22 @@
 
 set -e
 
+function to_cjs() {
+  mv napi/index.js napi/index.cjs
+  mv napi/index.d.ts napi/index.d.cts
+}
+
 if [ -z "$RUST_TARGET" ]; then
-  echo "RUST_TARGET is not set"
-  exit 1
+  echo "RUST_TARGET is not set. Building for current platform (DEBUG MODE)"
+  yarn napi build --platform napi
+  to_cjs
+else
+  echo "Building for $RUST_TARGET"
+  rustup target add $RUST_TARGET
+  yarn napi build --platform --target $RUST_TARGET --release napi
+  to_cjs
 fi
 
-echo "Building for $RUST_TARGET"
-rustup target add $RUST_TARGET
-yarn napi build --platform --target $RUST_TARGET --release napi
 yarn tsup
 
 echo "Done!"
