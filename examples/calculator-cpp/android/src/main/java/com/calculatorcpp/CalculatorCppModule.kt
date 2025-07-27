80cad72d6ca58c13f7a837a60b1899b76d00f867
leegeunhyeok <dev.ghlee@gmail.com>
2025-07-27 15:53:20 +0900

    chore: scaffold lib

---
+++ examples/calculator-cpp/android/src/main/java/com/calculatorcpp/CalculatorCppModule.kt
@@ -0,0 +1,42 @@
+package com.calculatorcpp
+
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.module.annotations.ReactModule
+
+@ReactModule(name = CalculatorModule.NAME)
+class CalculatorCppModule(reactContext: ReactApplicationContext) :
+  NativeCalculatorCppSpec(reactContext) {
+
+  init {
+    System.loadLibrary("crabycalculatorcpp")
+  }
+
+  private external fun nativeAdd(a: Double, b: Double): Double
+  private external fun nativeSubtract(a: Double, b: Double): Double
+  private external fun nativeMultiply(a: Double, b: Double): Double
+  private external fun nativeDivide(a: Double, b: Double): Double
+
+  override fun getName(): String {
+    return NAME
+  }
+
+  override fun add(a: Double, b: Double): Double {
+    return nativeAdd(a, b);
+  }
+
+  override fun subtract(a: Double, b: Double): Double {
+    return nativeSubtract(a, b);
+  }
+
+  override fun multiply(a: Double, b: Double): Double {
+    return nativeMultiply(a, b);
+  }
+
+  override fun divide(a: Double, b: Double): Double {
+    return nativeDivide(a, b);
+  }
+
+  companion object {
+    const val NAME = "Calculator"
+  }
+}
---
+++ examples/calculator-cpp/lib/module/NativeCalculator.js
@@ -0,0 +1,5 @@
+"use strict";
+
+import { TurboModuleRegistry } from 'react-native';
+export default TurboModuleRegistry.getEnforcing('Calculator');
+//# sourceMappingURL=NativeCalculator.js.map
