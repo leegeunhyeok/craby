ca9b74bcbf85f29c7c263b00fc1f7798f69aa4b1
leegeunhyeok <dev.ghlee@gmail.com>
2025-08-06 22:53:04 +0900

    ci: update config

--- packages/cli/scripts/build.sh
+++ packages/cli/scripts/build.sh
@@ -4,11 +4,11 @@ set -e
 
 if [ -z "$RUST_TARGET" ]; then
   echo "RUST_TARGET is not set. Building for current platform (DEBUG MODE)"
-  yarn napi build --platform --esm --output-dir napi
+  yarn napi build --platform --esm --output-dir napi $NAPI_FLAGS
 else
   echo "Building for $RUST_TARGET"
   rustup target add $RUST_TARGET
-  yarn napi build --platform --target $RUST_TARGET --esm --output-dir napi --release
+  yarn napi build --platform --target $RUST_TARGET --esm --output-dir napi --release $NAPI_FLAGS
 fi
 
 yarn tsup
