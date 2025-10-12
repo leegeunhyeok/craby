28bbecdea510ce4d355667a8dc18e9a8353b449c
leegeunhyeok <dev.ghlee@gmail.com>
2025-10-12 17:39:42 +0900

    update

--- examples/0.80/react-native.config.js
+++ examples/0.80/react-native.config.js
@@ -2,10 +2,8 @@ const path = require('node:path');
 
 module.exports = {
   dependencies: {
-    ...(process.env.E2E === '1' ? null : {
-      'craby-test': {
-        root: path.resolve(__dirname, '../craby-test'),
-      },
-    }),
+    'craby-test': {
+      root: path.resolve(__dirname, '../craby-test'),
+    },
   },
 };
