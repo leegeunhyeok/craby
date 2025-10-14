582843c15b75f55aebbd1208118856942a3911cf
leegeunhyeok <dev.ghlee@gmail.com>
2025-10-15 04:43:11 +0900

    update

--- xtask/src/tasks/prepare.rs
+++ xtask/src/tasks/prepare.rs
@@ -3,7 +3,7 @@ use craby_build::constants::toolchain::BUILD_TARGETS;
 
 use crate::utils::run_command;
 
-const EXAMPLE_APP_NAMES: [&str; 2] = ["craby-0.76", "craby-0.80"];
+const EXCLUDE_PACKAGE_NAMES: [&str; 3] = ["craby-test", "craby-0.76", "craby-0.80"];
 
 pub fn run() -> Result<()> {
     println!("Preparing...");
@@ -22,12 +22,16 @@ pub fn run() -> Result<()> {
             "--all",
             "--topological-dev",
             "--exclude",
-            format!("{{{}}}", EXAMPLE_APP_NAMES.join(",")).as_str(),
+            format!("{{{}}}", EXCLUDE_PACKAGE_NAMES.join(",")).as_str(),
             "run",
             "build",
         ],
         None,
     )?;
+
+    // Build JS bundle only for `craby-test`
+    // because `build (craby build)` command requires macOS and Xcode
+    run_command("yarn", &["workspace", "craby-test", "build:js"], None)?;
 
     println!("Prepare completed");
 
