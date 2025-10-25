2028dec0d3c72f51eac19a924aa6fdb30f003d28
leegeunhyeok <dev.ghlee@gmail.com>
2025-10-26 02:07:55 +0900

    fix

--- packages/craby-devkit/src/metro/config.ts
+++ packages/craby-devkit/src/metro/config.ts
@@ -5,18 +5,12 @@ interface GetMetroConfigOptions {
   resolverOptions?: Omit<CreateResolverOptions, 'rootPath'>;
 }
 
-type MetroConfig = any;
-
 export function getMetroConfig(rootDir: string, options: GetMetroConfigOptions) {
-  return (previousConfig: MetroConfig) => {
-    return {
-      ...previousConfig,
-      projectRoot: rootDir,
-      watchFolders: [...(previousConfig.watchFolders ?? []), getWorkspaceRoot(rootDir)],
-      resolver: {
-        ...previousConfig.resolver,
-        resolveRequest: createResolver({ rootPath: rootDir, ...options?.resolverOptions }),
-      },
-    };
+  return {
+    projectRoot: rootDir,
+    watchFolders: [getWorkspaceRoot(rootDir)],
+    resolver: {
+      resolveRequest: createResolver({ rootPath: rootDir, ...options?.resolverOptions }),
+    },
   };
 }
