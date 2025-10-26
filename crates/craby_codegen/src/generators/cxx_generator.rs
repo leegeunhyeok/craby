4708e955d9131a4838d561a8adf700b442b36921
leegeunhyeok <dev.ghlee@gmail.com>
2025-10-27 08:59:40 +0900

    update codegen

--- crates/craby_codegen/src/generators/cxx_generator.rs
+++ crates/craby_codegen/src/generators/cxx_generator.rs
@@ -595,11 +595,11 @@ impl CxxTemplate {
     ///
     /// class ThreadPool {
     /// private:
-    ///   std::vector<std::thread> workers;
-    ///   std::queue<std::function<void()>> tasks;
+    ///   bool stop;
     ///   std::mutex mutex;
     ///   std::condition_variable condition;
-    ///   bool stop;
+    ///   std::queue<std::function<void()>> tasks;
+    ///   std::vector<std::thread> workers;
     /// }
     ///
     /// public:
@@ -656,14 +656,7 @@ impl CxxTemplate {
     ///   }
     ///
     ///   ~ThreadPool() {
-    ///     {
-    ///       std::unique_lock<std::mutex> lock(mutex);
-    ///       stop = true;
-    ///     }
-    ///     condition.notify_all();
-    ///     for (std::thread &worker : workers) {
-    ///       worker.join();
-    ///     }
+    ///     shutdown();
     ///   }
     /// };
     ///
@@ -694,11 +687,11 @@ impl CxxTemplate {
 
             class ThreadPool {{
             private:
-              std::vector<std::thread> workers;
-              std::queue<std::function<void()>> tasks;
+              bool stop;
               std::mutex mutex;
               std::condition_variable condition;
-              bool stop;
+              std::queue<std::function<void()>> tasks;
+              std::vector<std::thread> workers;
 
             public:
               ThreadPool(size_t num_threads = 10) : stop(false) {{
@@ -755,14 +748,7 @@ impl CxxTemplate {
               }}
 
               ~ThreadPool() {{
-                {{
-                  std::unique_lock<std::mutex> lock(mutex);
-                  stop = true;
-                }}
-                condition.notify_all();
-                for (std::thread &worker : workers) {{
-                  worker.join();
-                }}
+                shutdown();
               }}
             }};
 
